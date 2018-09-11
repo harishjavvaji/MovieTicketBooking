4bce2331ddc734d42e6c6ea16d4d91f3fab1fa06
SukanyaVanka <sukanyavanka@gmail.com>
2018-09-11 14:24:13 -0400

    Created Controller,Repositories,Services packages

--- src/main/java/com/movietickets/booking/repositories/TicketRepository.java
+++ src/main/java/com/movietickets/booking/repositories/TicketRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository extends CrudRepository<Ticket,Integer> {
+public interface TicketRepository extends CrudRepository<Ticket,String> {
 }
